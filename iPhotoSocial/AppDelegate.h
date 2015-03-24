0bb1e291aca7bf119cabef3531eb687b190512b4
MAXToooNG <maxtooong@MAXToooNGdeMacBook-Pro.local>
2015-03-24 18:15:38 +0800

    Initial Commit

---
+++ iPhotoSocial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhotoSocial
+//
+//  Created by MAXToooNG on 15/3/24.
+//  Copyright (c) 2015年 Chen.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPhotoSocial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhotoSocial
+//
+//  Created by MAXToooNG on 15/3/24.
+//  Copyright (c) 2015年 Chen.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
