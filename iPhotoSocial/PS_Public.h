cfd6535031df36fa4f38de211f6eb6c32b4f870a
MAXToooNG <maxtooong@MAXToooNGdeMacBook-Pro.local>
2015-03-25 12:13:03 +0800

    commit .pch

---
+++ iPhotoSocial/PS_Public.h
@@ -0,0 +1,13 @@
+//
+//  PS_Public.h
+//  iPhotoSocial
+//
+//  Created by MAXToooNG on 15/3/25.
+//  Copyright (c) 2015年 Chen.Liu. All rights reserved.
+//
+
+#ifndef iPhotoSocial_PS_Public_h
+#define iPhotoSocial_PS_Public_h
+#define kWindowHeight [UIScreen mainScreen].bounds.size.height
+
+#endif
